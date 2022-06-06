4520e37766ff54716406fc71f6188420fad07631
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-06 14:59:42 +0530

    feat : card component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { CategoriesFilter } from "./components/CategoriesFilter";
+import { Card } from "./components/Card";
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [currentCategory, setCurrentCategory] = useState("🔥 24h Trending");
+  const filteredData = cards?.filter(
+    (item) => item.category === currentCategory
+  );
+
+  const selectCategoryHandler = (value) => {
+    setCurrentCategory(value);
+  };
+
+  useEffect(() => {
+    fetch("https://629dbb763dda090f3c094f11.mockapi.io/cards")
+      .then((res) => res.json())
+      .then((data) => {
+        setCards(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-4">
+      <div className="text-4xl ml-4 mt-1 font-bold">Live Spaces</div>
+      <div className="p-2 ml-2 mt-2">
+        ✅ All NFTs on Cyber either belong to or were minted by their space
+        creater.
+      </div>
+      <CategoriesFilter
+        selectCategoryHandler={selectCategoryHandler}
+        currentCategory={currentCategory}
+      />
+      <Card filteredData={filteredData} />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const Card = ({ filteredData }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mx-2 grid-flow-row auto-rows-auto">
+      {filteredData?.map(({ id, title, photoURL, tags }) => {
+        return (
+          <div className="relative" key={id}>
+            <img
+              src={photoURL}
+              alt="random-img"
+              className="h-70 object-cover rounded-lg"
+            />
+            <div className="bg-black text-white absolute w-full bottom-0 h-30 rounded-b-lg p-2 hover:opacity-90">
+              <span className="text-2xl m-2 p-2">{title}</span>
+              <div className="flex overflow-x-scroll webkit">
+                {tags.map(({ hash, description }) => {
+                  return (
+                    <div className="bg-gray-800 rounded-md h-12 w-32 my-4 px-2 ml-4">
+                      {hash.length > 10 ? hash.slice(0, 11) + "..." : hash}
+                      <div>
+                        {description.length > 10
+                          ? description.slice(0, 11) + "..."
+                          : description}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
